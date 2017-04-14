d0374d889324f17508167c9cebae930b6c6c54a8
Your Name <megan.a.dumas@gmail.com>
2017-04-14 15:22:48 -0700

    Initial Commit

---
+++ uiScrollingExampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uiScrollingExampleProject
+//
+//  Created by Megan Dumas on 4/14/17.
+//  Copyright © 2017 Megan Dumas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
